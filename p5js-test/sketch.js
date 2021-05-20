b1fdada1ec23cfbd1840f1dd86d00f042ff67dae
Paul Wheeler <paul@invioinc.com>
2021-05-20 13:09:53 -1000

    Try using the Promise returned by userStartAudio()

--- p5js-test/sketch.js
+++ p5js-test/sketch.js
@@ -7,19 +7,13 @@ function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
   startButton = createButton('Start!');
   startButton.position(width / 2, height / 2);
-  startButton.mousePressed(() => {
+  startButton.mousePressed(async () => {
     startButton.remove();
-    userStartAudio(
-	    cnv,
-      ctx => {
-        console.log(ctx);
-        fft = new p5.FFT();
-        input = new p5.AudioIn();
-        input.start();
-        fft.setInput(input);
-        input.start();
-      }
-    );
+    await userStartAudio();
+    fft = new p5.FFT();
+    input = new p5.AudioIn();
+    input.start();
+    fft.setInput(input);
   });
 }
 
