52c414122f89e02b4605abac31039295080aff25
Paul Wheeler <paul@invioinc.com>
2021-05-20 12:53:07 -1000

    Misc fixes for p5js test pages.

--- p5js-test/sketch.js
+++ p5js-test/sketch.js
@@ -1,62 +1,67 @@
 
 let fft;
 let input;
+let startButton;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-
+  createCanvas(windowWidth, windowHeight);
+  startButton = createButton('Start!');
+  startButton.position(width / 2, height / 2);
+  startButton.mousePressed(() => {
 	fft = new p5.FFT();
 	input = new p5.AudioIn();
+	input.start();
 	fft.setInput(input);
-}
-
-function mouseClicked() {
-  input.start();
+	input.start();
+	startButton.remove();
+  });
 }
 
 function draw() {
-	background('black');
-	let w = width / 2;
-	drawSpectrumGraph(0, 0, w, height);
-	drawWaveformGraph(w, 0, w, height);
+  background('black');
+  if (fft && input) {
+    let w = width / 2;
+    drawSpectrumGraph(0, 0, w, height);
+    drawWaveformGraph(w, 0, w, height);
+  }
 }
 
 // Graphing code adapted from https://jankozeluh.g6.cz/index.html by Jan Koželuh
 function drawSpectrumGraph(left, top, w, h) {
-	let spectrum = fft.analyze();
+  let spectrum = fft.analyze();
 
-	stroke('limegreen');
-	fill('darkgreen');
-	strokeWeight(1);
+  stroke('limegreen');
+  fill('darkgreen');
+  strokeWeight(1);
 
-	beginShape();
-	vertex(left, top + h);
+  beginShape();
+  vertex(left, top + h);
 
-	for (let i = 0; i < spectrum.length; i++) {
-		vertex(
-			left + map(log(i), 0, log(spectrum.length), 0, w),
-			top + map(spectrum[i], 0, 255, h, 0)
-		);
-	}
+  for (let i = 0; i < spectrum.length; i++) {
+    vertex(
+      left + map(log(i), 0, log(spectrum.length), 0, w),
+      top + map(spectrum[i], 0, 255, h, 0)
+    );
+  }
 
-	vertex(left + w, top + h);
-	endShape(CLOSE);
+  vertex(left + w, top + h);
+  endShape(CLOSE);
 }
 
 function drawWaveformGraph(left, top, w, h) {
-	let waveform = fft.waveform();
+  let waveform = fft.waveform();
 
-	stroke('limegreen');
-	noFill();
-	strokeWeight(1);
+  stroke('limegreen');
+  noFill();
+  strokeWeight(1);
 
-	beginShape();
+  beginShape();
 
-	for (let i = 0; i < waveform.length; i++) {
-		let x = map(i * 5, 0, waveform.length, 0, w);
-		let y = map(waveform[i], -1, 2, h / 10 * 8, 0);
-		vertex(left + x, top + y);
-	}
+  for (let i = 0; i < waveform.length; i++) {
+    let x = map(i * 5, 0, waveform.length, 0, w);
+    let y = map(waveform[i], -1, 2, h / 10 * 8, 0);
+    vertex(left + x, top + y);
+  }
 
-	endShape();
+  endShape();
 }
