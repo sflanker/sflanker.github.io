d0dc924bb58a204b08f7b5a7eaa083e77481cc7e
Paul Wheeler <paul@invioinc.com>
2021-05-20 13:05:34 -1000

    Added call to userStartAudio hopefully to resolve glitches accessing the mic.

--- p5js-test/sketch.js
+++ p5js-test/sketch.js
@@ -4,16 +4,22 @@ let input;
 let startButton;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
   startButton = createButton('Start!');
   startButton.position(width / 2, height / 2);
   startButton.mousePressed(() => {
-	fft = new p5.FFT();
-	input = new p5.AudioIn();
-	input.start();
-	fft.setInput(input);
-	input.start();
-	startButton.remove();
+    startButton.remove();
+    userStartAudio(
+	    cnv,
+      ctx => {
+        console.log(ctx);
+        fft = new p5.FFT();
+        input = new p5.AudioIn();
+        input.start();
+        fft.setInput(input);
+        input.start();
+      }
+    );
   });
 }
 
